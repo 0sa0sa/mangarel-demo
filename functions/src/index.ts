b4a47dab77284fc84150f86acf60b17edc84d6f6
osasou <osasou0125@gmail.com>
2022-05-01 22:37:13 +0900

    create cloud functions for crawling

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,20 +1,47 @@
 import * as functions from 'firebase-functions';
 import admin from 'firebase-admin';
-import { collectionName } from './services/mangarel/constants';
+import puppeteer from 'puppeteer';
+
+// import { collectionName } from './services/mangarel/constants';
+import { feedCalendar } from './crawlers/kodansha-calendar';
+import { saveFeedMemo } from './firestore-admin/feed-memo';
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
 
+const PUPPETEER_OPTIONS = {
+  args: [
+    '--disable-gpu',
+    '--disable-dev-shm-usage',
+    '--disable-setuid-sandbox',
+    '--no-first-run',
+    '--no-sandbox',
+    '--no-zygote',
+    '--single-process',
+  ],
+  headless: true,
+};
+
 admin.initializeApp();
 
-export const publishers = functions
+export const fetchCalendar = functions
   .region('asia-northeast1')
-  .https.onRequest(async (req, res) => {
-    const snap = await admin
-      .firestore()
-      .collection(collectionName.publishers)
-      .get();
-    const data = snap.docs.map((doc) => doc.data());
-    res.send({ data });
+  .runWith({
+    timeoutSeconds: 300,
+    memory: '2GB',
+  })
+  .pubsub.schedule('0 2 1,10,20 * *')
+  .timeZone('Asia/Tokyo')
+  .onRun(async () => {
+    const browser = await puppeteer.launch(PUPPETEER_OPTIONS);
+    const page = await browser.newPage();
+    const db = admin.firestore();
+
+    const memos = await feedCalendar(page);
+    const fetchCount = await saveFeedMemo(db, memos, 'kodansha');
+
+    await browser.close();
+
+    console.log(`Fetched Kodansha calendar. Wrote ${fetchCount} memos.`);
   });
